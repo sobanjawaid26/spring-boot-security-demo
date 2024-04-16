caf00b09ac210b4ecd9972751f02ef2373fcf94a
sobanjawaid26 <sobanjawaid26@gmail.com>
2024-04-16 18:23:30 +0530

    user anna amith created

--- src/main/java/com/securitystarter/securitystarter/security/ApplicationSecurityConfig.java
+++ src/main/java/com/securitystarter/securitystarter/security/ApplicationSecurityConfig.java
@@ -1,9 +1,16 @@
 package com.securitystarter.securitystarter.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static org.springframework.security.core.userdetails.User.*;
 
 
 @Configuration
@@ -22,5 +29,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmithUser = builder()
+                .username("annasmith")
+                .password("password")
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(annaSmithUser);
     }
 }
