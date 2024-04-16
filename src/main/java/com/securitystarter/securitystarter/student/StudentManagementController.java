857c0f649d176f4b0b129fad10bfa33f31ed5985
sobanjawaid26 <sobanjawaid26@gmail.com>
2024-04-16 21:48:11 +0530

    authority changed to @PreAuthorize in APIs instead of antMatchers in security config

--- src/main/java/com/securitystarter/securitystarter/student/StudentManagementController.java
+++ src/main/java/com/securitystarter/securitystarter/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.securitystarter.securitystarter.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -16,21 +17,25 @@ public class StudentManagementController {
     );
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents(){
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student){
         System.out.println("Saving student : " + student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId){
         System.out.println("Delete Api called for student with id : " +studentId);
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student){
         System.out.println(String.format("Update API called : %s %s", studentId, student));
     }
